30a0f5841f6f5b0f875e950a7afe10d35e6bd4f5
Nicholas Kirk <Nick@Mac-mini.local>
2021-11-15 22:12:55 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,57 @@
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const circles = document.querySelectorAll('.circle');
+
+let currentActive = 1;
+
+next.addEventListener('click', function(){
+    currentActive++
+
+    if (currentActive > circles.length) {
+        currentActive = circles.length;
+    }
+
+    updateProgress()
+
+})
+
+prev.addEventListener('click', function(){
+    currentActive--
+
+    if (currentActive < 1) {
+        currentActive = 1;
+    }
+
+    updateProgress()
+
+})
+
+function updateProgress() {
+    circles.forEach( (circle, index) => {
+        if (index < currentActive) {
+            circle.classList.add('active');
+        } else {
+            circle.classList.remove('active');
+        }
+    })
+
+    const actives = document.querySelectorAll('.active');
+
+    progress.style.width = (actives.length -1 ) / (circles.length -1) * 100 + '%';
+
+    if (currentActive === 1) {
+        prev.disabled = true;
+    } else if (currentActive == circles.length) {
+        next.disabled = true;
+    }
+    else {
+        next.disabled = false;
+        prev.disabled = false;
+    }
+
+    if (actives.length == circles.length) {
+        alert('Congratulations you finished!')
+    }
+
+}
